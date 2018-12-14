ed3d7bdd73d09e1d10c99a5f1643a47666af317a
Rakesh Menon <rmenon3@sapient.com>
2018-12-14 10:28:03 +0530

    testing auto publish in netlify

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Publicis Sapient - XT hiring challenge!!</h1>
+        <h1>Publicis Sapient - XT hiring challenge!</h1>
         <p>
           Edit
           {' '}
